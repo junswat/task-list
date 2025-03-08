8eb228948f87ee4e5a766d44290c436d5e15fd19
Junswat <tocotoco2674@gmail.com>
2025-03-09 01:56:15 +0900

    Fix MIME type issues and optimize build configuration

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,13 @@ module.exports = defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
+      }
+    }
   },
   resolve: {
     alias: {
