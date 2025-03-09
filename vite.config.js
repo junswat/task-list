8d6914f5c842a6d9647c836d7021ecd508b9fd6f
Junswat <tocotoco2674@gmail.com>
2025-03-09 11:37:47 +0900

    � MIMEタイプの問題を修正

--- vite.config.js
+++ vite.config.js
@@ -13,8 +13,9 @@ module.exports = defineConfig({
         main: path.resolve(__dirname, 'index.html')
       },
       output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
+        format: 'es',
+        entryFileNames: 'assets/[name].[hash].mjs',
+        chunkFileNames: 'assets/[name].[hash].mjs',
         assetFileNames: 'assets/[name].[hash].[ext]'
       }
     }
